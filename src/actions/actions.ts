50a1ab1e477c54866444506521ca256ee1891837
Krishan <krishandwive9@gmail.com>
2025-04-29 23:58:11 +0530

    added a way to protect routes and server actions

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,9 +1,17 @@
 "use server";
 
 import { prisma } from "@/lib/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export async function addExpense(formData: FormData) {
+  const { isAuthenticated } = getKindeServerSession();
+
+  if (!(await isAuthenticated())) {
+    return redirect("/");
+  }
+
   await prisma.expense.create({
     data: {
       amount: Number(formData.get("amount")),
@@ -14,6 +22,11 @@ export async function addExpense(formData: FormData) {
 }
 
 export async function deleteExpense(id: number) {
+  const { isAuthenticated } = getKindeServerSession();
+
+  if (!(await isAuthenticated())) {
+    return redirect("/");
+  }
   await prisma.expense.delete({
     where: {
       id: id,
--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,10 +1,20 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const Account = () => {
+const Account = async () => {
+  const { isAuthenticated, getUser } = getKindeServerSession();
+
+  if (!(await isAuthenticated())) {
+    return redirect("/");
+  }
+
+  const userInfo = await getUser();
+
   return (
     <div className=" flex flex-col items-center">
       <h1 className="text-3xl font-bold">Account</h1>
-      <p className="mt-4">Logged in as :</p>
+      <p className="mt-4">Logged in as: {userInfo?.email}</p>
     </div>
   );
 };
