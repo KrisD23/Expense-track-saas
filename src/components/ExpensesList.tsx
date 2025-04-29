ac78687ce1575127db85992a3a82aca07bce699f
Krishan <krishandwive9@gmail.com>
2025-04-29 18:32:27 +0530

    fixed the primsa client import problem also getting the list of expense

--- src/components/ExpensesList.tsx
+++ src/components/ExpensesList.tsx
@@ -1,17 +1,12 @@
+import { prisma } from "@/lib/db";
 import React from "react";
 
-const ExpensesList = () => {
-  const expense = [
-    {
-      id: 1,
-      description: "Rent",
-      amount: 100,
-    },
-  ];
+const ExpensesList = async () => {
+  const expenses = await prisma.expense.findMany();
 
   return (
     <ul className="h-[300px] bg-white rounded mt-4 shadow-md px-2">
-      {expense.map((expense) => (
+      {expenses.map((expense) => (
         <li
           key={expense.id}
           className="flex justify-between py-2 px-4 border-b border-gray-200 hover:bg-gray-100"
