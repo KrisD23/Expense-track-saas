1043ca99c326c8873ff0412c2fe657b58d9394a5
Krishan <krishandwive9@gmail.com>
2025-04-29 17:50:01 +0530

    modified expense list view

--- src/components/ExpensesList.tsx
+++ src/components/ExpensesList.tsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const ExpensesList = () => {
-  return <ul className="h-[300px] bg-white rounded mt-4 shadow-md px-2"></ul>;
+  const expense = [
+    {
+      id: 1,
+      description: "Rent",
+      amount: 100,
+    },
+  ];
+
+  return (
+    <ul className="h-[300px] bg-white rounded mt-4 shadow-md px-2">
+      {expense.map((expense) => (
+        <li
+          key={expense.id}
+          className="flex justify-between py-2 px-4 border-b border-gray-200 hover:bg-gray-100"
+        >
+          <p>{expense.description}</p>
+          <div className="flex gap-2">
+            <p className="font-bold ">{expense.amount}</p>
+            <button className="text-red-500 border border-red-500 px-2">
+              delete
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default ExpensesList;
