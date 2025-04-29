e0cd58d26b886dede395cfed4f24c5457c5ab1ed
Krishan <krishandwive9@gmail.com>
2025-04-29 17:54:37 +0530

    edited account page

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Account = () => {
-  return <div>Account</div>;
+  return (
+    <div className=" flex flex-col items-center">
+      <h1 className="text-3xl font-bold">Account</h1>
+      <p className="mt-4">Logged in as :</p>
+    </div>
+  );
 };
 
 export default Account;
