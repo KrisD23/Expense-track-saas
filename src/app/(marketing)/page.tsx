debd775f9b73a8433994ec0d27d4e42d1336b268
Krishan <krishandwive9@gmail.com>
2025-04-29 16:02:53 +0530

    defined the folder structure

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="justify-center items-center flex h-screen">
+        <h1 className="font-bold text-4xl">Manage all your expenses</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
